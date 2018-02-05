4f06277f9bb217b7932967a84c6e468a96d90f6f
inoble <iannoble@gmail.com>
2018-02-05 19:38:54 +0000

    Update snapshot_db.py

--- source/snapshot_db.py
+++ source/snapshot_db.py
@@ -1,15 +1,14 @@
 import dbf, operator, os
 
 def snapshot_db(db_id):
-	""" Creates a snapshot of the most recently edited database available as a list """
+	""" Creates a snapshot of a database """
 
 	# location of NBA Live 06 saved games
 	target_dir = r'C:\Users\Ian\Documents\NBA Live 06\saves'
-
-	# list to contain each directory within target_dir
+	
 	directory_list = []
 
-	# add items to directory_list
+	# create a list of all directories available at location target_dir
 	for d in os.listdir(target_dir):
 		bd = os.path.join(target_dir, d)
 		if os.path.isdir(bd): 
@@ -21,21 +20,21 @@ def snapshot_db(db_id):
 	# sort in order of which directory was last modified
 	directory_list_sorted = sorted(directory_list, key=operator.itemgetter(1), reverse=True)
 
-	# find most recently modified directory
+	# find directory specified by db_id (0 = most recently modified, 1 = next most recent)
 	dirstr = str(directory_list_sorted[db_id][0])
 	
 	# modify the string of the most recently modified directory to contain only the location
 	last = dirstr.strip("'[]")
 	
-	# point towards the most recently edited players.dbf database
+	# point towards the directory's players.dbf database
 	location = (last + '\\' + 'players.dbf')
 
-	# assign most recently modified players.dbf to the dbf module 
+	# assign players.dbf to be processed by the dbf module 
 	snapshot_database = dbf.Table(location)
 	
 	snapshot_db = []
 	
-	# Convert snapshot_database to a list using dbf module
+	# Convert snapshot_database to a list of useful data using dbf module
 	with snapshot_database:
 		
 		index = snapshot_database.create_index (lambda rec: (rec.team))
@@ -49,29 +48,29 @@ def snapshot_db(db_id):
 			for item in matches:
 				
 				snapshot_db.append([
-										item[45],		# Team [0]
-										item[2],		# First Name [1]
-										item[1],		# Surname [2]
-										item[46],		# Roster Position [3]
+										item[45],							# Team [0]
+										item[2],							# First Name [1]
+										item[1],							# Surname [2]
+										item[46],							# Roster Position [3]
 										
-										item[122],		# Minutes [4]
-										item[121],		# Turnovers [5]
-										item[115],		# Blocks [6]
-										item[119],		# Steals [7]
+										item[122],							# Minutes [4]
+										item[121],							# Turnovers [5]
+										item[115],							# Blocks [6]
+										item[119],							# Steals [7]
 										format(item[116] + item[120]),		# Rebounds [8]
-										item[117],		# Assists [9]
+										item[117],							# Assists [9]
 										format(((item[110]-item[112])*2) + (item[112]*3) + (item[114])),
-														# Points [10]
-										item[110],		# Field Goals [11]
-										item[109],		# Field Goals Attempted [12]
-										item[112],		# 3PT [13]
-										item[111],		# 3PT Attempted [14]
-										item[114],		# Free Throws [15]
-										item[113],		# Free Throws Attempted [16]
+																			# Points [10]
+										item[110],							# Field Goals [11]
+										item[109],							# Field Goals Attempted [12]
+										item[112],							# 3PT [13]
+										item[111],							# 3PT Attempted [14]
+										item[114],							# Free Throws [15]
+										item[113],							# Free Throws Attempted [16]
 										
-										item[123],		# Games Played [17]
-										item[4],		# Player ID [18]
-										item[118],		# Personal Fouls [19]
+										item[123],							# Games Played [17]
+										item[4],							# Player ID [18]
+										item[118],							# Personal Fouls [19]
 										])
 				
 			team = team + 1
@@ -80,5 +79,8 @@ def snapshot_db(db_id):
 	snapshot_db_sorted = sorted(snapshot_db, key=operator.itemgetter(18))
 	
 	return snapshot_db_sorted
-	
-#snapshot_db()
+
+
+if __name__ == "__main__":
+
+	snapshot_db(db_id)
