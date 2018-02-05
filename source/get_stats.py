82ba7a1c93f1c185a323ef953ad119cfa4622586
inoble <iannoble@gmail.com>
2018-02-05 19:32:54 +0000

    Update get_stats.py

--- source/get_stats.py
+++ source/get_stats.py
@@ -1,9 +1,11 @@
 import operator
-import starting_db, ending_db
+import snapshot_db
 
-def get_stats():	
-	start_db = starting_db.starting_db()
-	end_db = ending_db.ending_db()
+def get_stats():
+	""" Creates every team's statistics for each game by subtracting an older from a newer database of stats """
+
+	start_db = snapshot_db.snapshot_db(1)
+	end_db = snapshot_db.snapshot_db(0)
 	
 	total_rows = len(start_db)
 	
@@ -14,29 +16,26 @@ def get_stats():
 	for x in range(total_rows):
 	
 		subtracted.append([
-						end_db[x][0],
-						end_db[x][1],
-						end_db[x][2],
-						end_db[x][3],
-						format(int(end_db[x][4]) - int(start_db[x][4])),
-						format(int(end_db[x][5]) - int(start_db[x][5])),
-						format(int(end_db[x][6]) - int(start_db[x][6])),
-						format(int(end_db[x][7]) - int(start_db[x][7])),
-						format(int(end_db[x][8]) - int(start_db[x][8])),
-						format(int(end_db[x][9]) - int(start_db[x][9])),
-						format(int(end_db[x][10]) - int(start_db[x][10])),
-						format(int(end_db[x][11]) - int(start_db[x][11])),
-						format(int(end_db[x][12]) - int(start_db[x][12])),
-						format(int(end_db[x][13]) - int(start_db[x][13])),
-						format(int(end_db[x][14]) - int(start_db[x][14])),
-						format(int(end_db[x][15]) - int(start_db[x][15])),
-						format(int(end_db[x][16]) - int(start_db[x][16])),
-						end_db[x][18],
-						format(int(end_db[x][19]) - int(start_db[x][19])), # Personal Fouls [18]
+						end_db[x][0],	# Team ID
+						end_db[x][1],	# Player First Name
+						end_db[x][2],	# Player Surname
+						end_db[x][3],	# Player Roster Position
+						format(int(end_db[x][4]) - int(start_db[x][4])),	# Minutes Played
+						format(int(end_db[x][5]) - int(start_db[x][5])),	# Turnovers
+						format(int(end_db[x][6]) - int(start_db[x][6])),	# Blocks
+						format(int(end_db[x][7]) - int(start_db[x][7])),	# Steals
+						format(int(end_db[x][8]) - int(start_db[x][8])),	# Rebounds
+						format(int(end_db[x][9]) - int(start_db[x][9])),	# Assists
+						format(int(end_db[x][10]) - int(start_db[x][10])),	# Points
+						format(int(end_db[x][11]) - int(start_db[x][11])),	# Field Goals Made
+						format(int(end_db[x][12]) - int(start_db[x][12])),	# Field Goals Attempted
+						format(int(end_db[x][13]) - int(start_db[x][13])),	# Three-Pointers Made
+						format(int(end_db[x][14]) - int(start_db[x][14])),	# Three-Pointers Attempted
+						format(int(end_db[x][15]) - int(start_db[x][15])),	# Free Throws Made
+						format(int(end_db[x][16]) - int(start_db[x][16])),	# Free Throws Attempted
+						end_db[x][18],										# Player ID
+						format(int(end_db[x][19]) - int(start_db[x][19])), 	# Personal Fouls
 						])
-	
-	#test_subtracted_db = open(r'C:\Users\Ian\Desktop\Py\WIP\boxscores\subtracted.txt', 'w')
-	#test_subtracted_db.write ( str(subtracted) )
 	
 	subtracted_sorted = sorted(subtracted, key=operator.itemgetter(0, 3))
 	
@@ -44,4 +43,7 @@ def get_stats():
 	
 	return subtracted_sorted
 
-get_stats()
+
+if __name__ == "__main__":
+
+	get_stats()
