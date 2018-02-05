f0cbd02305682a66ec17ec6ca8aaad29f28094a9
inoble <iannoble@gmail.com>
2018-02-05 20:16:41 +0000

    Update get_in_game_date.py

--- source/get_in_game_date.py
+++ source/get_in_game_date.py
@@ -1,21 +1,22 @@
-import datetime
 import find_games
 
-def get_in_game_date(game_number):
-	games = find_games.find_games()
+def get_in_game_date(game_number, irl_date):
+	""" Finds the in-game date for which simulations are processed """
 
-	irl_date = datetime.date.today()
+	games = find_games.find_games()
+	
+	in_game_day = int((str(games[game_number][0])[5:-1])[2:])
+
 	in_game_month = int((str(games[game_number][0])[5:-1])[:-2])
 	
 	if in_game_month < 9:
+	
 		in_game_year = str(irl_date)[:-6]
+		
 	elif in_game_month > 9:
+	
 		in_game_year = int(str(irl_date)[:-6])-1
 	
-	in_game_date = str(in_game_year) + '-' + ((str(games[game_number][0])[5:-1])[:-2]) + '-' + ((str(games[game_number][0])[5:-1])[2:])
+	game_date = str(str(in_game_year) + '-' + str(in_game_month) + '-' + str(in_game_day))
 	
-	print (in_game_date)
-	
-	return in_game_date
-
-get_in_game_date()
+	return game_date
